5cee308c18176d604c4f6633ac50bb38908916aa
Eric Duong <eduong@email.arizona.edu>
2023-05-28 18:03:53 -0700

    get baseline task data

--- scripts/download_baseline_tasks_data.sh
+++ scripts/download_baseline_tasks_data.sh
@@ -3,22 +3,19 @@
 # SSH connection details
 remote_user="eduong"
 remote_host="gauss"
-data_location="/space/calebshibu/Neurips"
-data_output_dir="../data/raw/physio_data"
+data_location="/tomcat/data/raw/LangLab/experiments/study_3_pilot/group"
+data_output_dir="../data/raw/tasks"
 
 # List of folders to sync
 source ../.env
 
-# File extension to filter
-file_extension="_filtered.csv"
-
 # Iterate through the list of folders
 for folder in "${FOLDERS[@]}"; do
-    source="$remote_user@$remote_host:$data_location/$folder"
+    source="$remote_user@$remote_host:$data_location/$folder/baseline_tasks/"
     destination="$data_output_dir/$folder"
 
     mkdir -p "$destination"
 
     echo "Syncing $source to $destination"
-    rsync -avP --include="*/" --include="*$file_extension" --exclude="*" "$source" "$destination"
+    rsync -avP "$source" "$destination"
 done
