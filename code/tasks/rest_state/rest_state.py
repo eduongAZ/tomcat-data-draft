17b5f8e44aeb89b2f8a458e279b0aa0136a61d87
Eric Duong <eduong@email.arizona.edu>
2023-05-29 18:11:21 -0700

    Add relevant data

--- code/tasks/rest_state/rest_state.py
+++ code/tasks/rest_state/rest_state.py
@@ -4,15 +4,36 @@ from utils import read_csv_file
 from utils import read_json_file
 
 
+def _combine_rest_state_physio_task(rest_state_task_df: pd.DataFrame,
+                                    rest_state_physio_csv_path: str,
+                                    participant_id: str) -> pd.DataFrame:
+    rest_state_physio_df = read_csv_file(rest_state_physio_csv_path, delimiter='\t')
+
+    start_time = rest_state_task_df.query('event_type == "start_rest_state"').iloc[0]['time']
+    end_time = rest_state_task_df.query('event_type == "end_rest_state"').iloc[0]['time']
+
+    rest_state_physio_df = rest_state_physio_df.assign(participant_id=participant_id)
+
+    def _assign_event_type(row):
+        if start_time <= row['unix_time'] <= end_time:
+            return 'during_rest_state'
+        else:
+            return 'outside_of_rest_state'
+
+    rest_state_physio_df['task_status'] = rest_state_physio_df.apply(_assign_event_type, axis=1)
+
+    return rest_state_physio_df
+
+
 class RestState:
     def __init__(self,
                  participant_ids: dict,
                  rest_state_task_df: pd.DataFrame,
-                 rest_state_physio_df: dict[str, pd.DataFrame]
+                 rest_state_physio: dict[str, pd.DataFrame]
                  ):
         self.participant_ids = participant_ids
         self.rest_state_task_df = rest_state_task_df
-        self.rest_state_physio_df = rest_state_physio_df
+        self.rest_state_physio = rest_state_physio
 
     @classmethod
     def from_files(cls,
@@ -33,21 +54,25 @@ class RestState:
         # Read rest state task data
         rest_state_task_df = read_csv_file(rest_state_csv_path, delimiter=';')
 
-        # Read rest state physio data
-        lion_physio_nirs_df = read_csv_file(
-            rest_state_physio_directory + '/lion/NIRS_filtered_rest_state.csv',
-            delimiter='\t')
-        tiger_physio_nirs_df = read_csv_file(
-            rest_state_physio_directory + '/tiger/NIRS_filtered_rest_state.csv',
-            delimiter='\t')
-        leopard_physio_nirs_df = read_csv_file(
-            rest_state_physio_directory + '/leopard/NIRS_filtered_rest_state.csv',
-            delimiter='\t')
+        lion_physio_nirs_df = _combine_rest_state_physio_task(
+            rest_state_task_df=rest_state_task_df,
+            rest_state_physio_csv_path=rest_state_physio_directory + '/lion/NIRS_filtered_rest_state.csv',
+            participant_id=participant_ids['lion'])
+
+        tiger_physio_nirs_df = _combine_rest_state_physio_task(
+            rest_state_task_df=rest_state_task_df,
+            rest_state_physio_csv_path=rest_state_physio_directory + '/tiger/NIRS_filtered_rest_state.csv',
+            participant_id=participant_ids['tiger'])
+
+        leopard_physio_nirs_df = _combine_rest_state_physio_task(
+            rest_state_task_df=rest_state_task_df,
+            rest_state_physio_csv_path=rest_state_physio_directory + '/leopard/NIRS_filtered_rest_state.csv',
+            participant_id=participant_ids['leopard'])
 
         return cls(
             participant_ids=participant_ids,
             rest_state_task_df=rest_state_task_df,
-            rest_state_physio_df={
+            rest_state_physio={
                 'lion': lion_physio_nirs_df,
                 'tiger': tiger_physio_nirs_df,
                 'leopard': leopard_physio_nirs_df
