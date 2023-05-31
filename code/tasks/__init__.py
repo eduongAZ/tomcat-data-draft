0f18be3f8db75aa627ee603f6ead6f19dbffd4af
Eric Duong <eduong@email.arizona.edu>
2023-05-30 22:18:42 -0700

    competitive ping pong

--- code/tasks/__init__.py
+++ code/tasks/__init__.py
@@ -1,11 +1,13 @@
 from .affective_task_individual import AffectiveTaskIndividual
 from .affective_task_team import AffectiveTaskTeam
 from .finger_tapping import FingerTapping
+from .ping_pong_competitive import PingPongCompetitive
 from .rest_state import RestState
 
 __all__ = [
     "RestState",
     "FingerTapping",
     "AffectiveTaskIndividual",
-    "AffectiveTaskTeam"
+    "AffectiveTaskTeam",
+    "PingPongCompetitive"
 ]
