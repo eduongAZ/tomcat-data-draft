423b0b0fa4935fb084cfe44c7ac66dd26cca1368
Eric Duong <eduong@email.arizona.edu>
2023-06-01 22:48:53 -0700

    extract affective individual and finger tapping info

--- code/pipeline/__init__.py
+++ code/pipeline/__init__.py
@@ -1,5 +1,7 @@
-from .prepare import prepare_rest_state
+from .prepare import prepare_rest_state, prepare_finger_tapping, prepare_affective_task_individual
 
 __all__ = [
-    'prepare_rest_state'
+    'prepare_rest_state',
+    'prepare_finger_tapping',
+    'prepare_affective_task_individual'
 ]
--- code/pipeline/prepare/__init__.py
+++ code/pipeline/prepare/__init__.py
@@ -1,5 +1,9 @@
+from .prepare_affective_task_individual import prepare_affective_task_individual
+from .prepare_finger_tapping import prepare_finger_tapping
 from .prepare_rest_state import prepare_rest_state
 
 __all__ = [
-    'prepare_rest_state'
+    'prepare_rest_state',
+    'prepare_finger_tapping',
+    'prepare_affective_task_individual'
 ]
