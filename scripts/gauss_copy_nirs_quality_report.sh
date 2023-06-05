db6fbb77532abd92ca19b6d513995b3dadca03da
Eric Duong <eduong@email.arizona.edu>
2023-06-05 14:15:29 -0700

    gauss list processed experiments

---
+++ scripts/gauss_copy_nirs_quality_report.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify the path
+path="/tomcat/data/derived/drafts/draft_2023_06_05_11"
+
+# Check if the path exists and is a directory
+if [[ ! -d "$path" ]]; then
+  echo "Invalid path or directory does not exist."
+  exit 1
+fi
+
+# Retrieve a list of directory names
+directories=("$path"/*)
+
+# Iterate through the list and print each directory name
+for directory in "${directories[@]}"; do
+  if [[ -d "$directory" ]]; then
+    echo "$directory"
+  fi
+done
