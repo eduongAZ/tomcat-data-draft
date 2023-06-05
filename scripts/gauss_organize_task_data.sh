912e83ddbb4a8d9f472d5f5723e086b2dd087abf
Eric Duong <eduong@email.arizona.edu>
2023-06-05 11:47:42 -0700

    fix script on gauss

--- scripts/gauss_organize_task_data.sh
+++ scripts/gauss_organize_task_data.sh
@@ -10,13 +10,13 @@ echo "Organizing baseline tasks ..."
 
 # Iterate through the list of folders
 for folder in "${FOLDERS[@]}"; do
-    source="$data_location/$folder/baseline_tasks/*"
+    source="$data_location/$folder/baseline_tasks/"
     destination="$data_output_dir/$folder/"
 
     mkdir -p "$destination"
 
     echo "Syncing $source to $destination"
-    cp -r "$source" "$destination"
+    cp -r "$source"* "$destination"
 done
 
 echo "Organizing minecraft tasks ..."
