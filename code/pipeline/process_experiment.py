9c41583d736e36c385adc05b420e36b3f0416b0e
Eric Duong <eduong@email.arizona.edu>
2023-06-05 21:09:30 -0700

    test reststate

--- code/pipeline/process_experiment.py
+++ code/pipeline/process_experiment.py
@@ -34,16 +34,14 @@ def _process_rest_state(path_to_task: str,
             output_file=log_file
         )
 
-        print(rest_state_data)
+        rest_state = RestState.from_files(
+            rest_state_data['info'],
+            rest_state_data['task_csv_path'],
+            rest_state_data['physio_name_path'],
+            frequency=frequency
+        )
 
-        # rest_state = RestState.from_files(
-        #     rest_state_data['info'],
-        #     rest_state_data['task_csv_path'],
-        #     rest_state_data['physio_name_path'],
-        #     frequency=frequency
-        # )
-        #
-        # rest_state.write_physio_data_csv(output_path + '/' + experiment)
+        rest_state.write_physio_data_csv(output_path + '/' + experiment)
     except FileDoesNotExistError as e:
         log_file.write(str(e) + '\n')
 
