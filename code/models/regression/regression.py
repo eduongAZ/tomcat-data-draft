0fc55d3c45fced9758acffef88a2526b2a721da5
Eric Duong <eduong@email.arizona.edu>
2023-06-06 17:45:17 -0700

    analyze correlation on Gauss and generate tables

--- code/models/regression/regression.py
+++ code/models/regression/regression.py
@@ -6,7 +6,8 @@ def regression(physio_task_dir: str,
                physio_task_file_name: str,
                score_column_name: str,
                channels: list[str],
-               output_dir: str):
+               output_img_dir: str | None = None,
+               output_file_path: str | None = None):
     exp_files = find_files(physio_task_dir, physio_task_file_name)
     correlation_per_channel, scores = get_physio_correlations_and_scores(
         exp_files.values(),
@@ -14,13 +15,27 @@ def regression(physio_task_dir: str,
         score_column_name,
         remove_nan=True)
 
-    predictions_per_channel, rmse_per_channel, linear_regressor_per_channel = linear_fit(
-        correlation_per_channel, scores)
+    predictions_per_channel, \
+        rmse_per_channel, \
+        linear_regressor_per_channel, \
+        r2_per_channel, \
+        null_model_r2, \
+        linear_regression_slope_per_channel = \
+        linear_fit(
+            correlation_per_channel,
+            scores,
+            output_file_path)
 
-    print(rmse_per_channel)
+    if output_img_dir is not None:
+        plot_regression_per_channel(
+            correlation_per_channel,
+            scores,
+            linear_regressor_per_channel,
+            output_img_dir)
 
-    plot_regression_per_channel(
-        correlation_per_channel,
-        scores,
-        linear_regressor_per_channel,
-        output_dir)
+    return predictions_per_channel, \
+        rmse_per_channel, \
+        linear_regressor_per_channel, \
+        r2_per_channel, \
+        null_model_r2, \
+        linear_regression_slope_per_channel
--- code/models/regression/utils/linear_fit.py
+++ code/models/regression/utils/linear_fit.py
@@ -1,11 +1,14 @@
+import json
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
 
 def linear_fit(correlation_per_channel: dict[str, list[any]],
-               scores: list[any]
-               ) -> tuple[dict[str, list[any]], dict[str, float], dict[str, LinearRegression]]:
+               scores: list[any],
+               output_file: str | None = None):
     # ensure valid input
     for correlations in correlation_per_channel.values():
         assert len(correlations) == len(scores)
@@ -16,7 +19,11 @@ def linear_fit(correlation_per_channel: dict[str, list[any]],
     # fit a linear regression model for each channel
     rmse_per_channel = {}
     linear_regressor_per_channel = {}
+    linear_regression_slope_per_channel = {}
     predictions_per_channel = {}
+    r2_per_channel = {}
+
+    mean_score = np.mean(scores)  # for null model
 
     for i, (channel, values) in enumerate(correlation_per_channel.items()):
         lr = LinearRegression()
@@ -26,8 +33,30 @@ def linear_fit(correlation_per_channel: dict[str, list[any]],
 
         y_hat = lr.predict(X)
 
-        predictions_per_channel[channel] = y_hat
+        predictions_per_channel[channel] = y_hat.tolist()
         linear_regressor_per_channel[channel] = lr
         rmse_per_channel[channel] = np.sqrt(mean_squared_error(scores, y_hat))
+        r2_per_channel[channel] = r2_score(scores, y_hat)
+        linear_regression_slope_per_channel[channel] = lr.coef_[0]
 
-    return predictions_per_channel, rmse_per_channel, linear_regressor_per_channel
+    null_model_r2 = r2_score(scores, [mean_score] * len(scores))
+
+    # Log results to JSON file
+    if output_file is not None:
+        json_filename = output_file
+        json_data = {
+            'predictions_per_channel': predictions_per_channel,
+            'rmse_per_channel': rmse_per_channel,
+            'r2_per_channel': r2_per_channel,
+            'null_model_r2': null_model_r2,
+            'linear_regression_slope': linear_regression_slope_per_channel
+        }
+        with open(json_filename, 'w') as jsonfile:
+            json.dump(json_data, jsonfile, indent=4)
+
+    return predictions_per_channel, \
+        rmse_per_channel, \
+        linear_regressor_per_channel, \
+        r2_per_channel, \
+        null_model_r2, \
+        linear_regression_slope_per_channel
