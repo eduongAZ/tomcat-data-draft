bcdb678d1f76801407bab90bd44d81a98bac2198
Eric Duong <eduong@arizona.edu>
2023-06-11 15:28:09 -0700

    Prepare minecraft

--- code/prepare/prepare_task_data.py
+++ code/prepare/prepare_task_data.py
@@ -120,17 +120,23 @@ def prepare_task_data(task_data_path: str,
             if status:
                 experiments_tasks_data[physio_type][experiment]['ping_pong_cooperative'] = task_data
 
-            string_stream.write(f'[{experiment}] Preparing minecraft training\n')
+            # Prepare Minecraft missions
+            string_stream.write(f'[{experiment}] Preparing minecraft missions\n')
+            task_data, status, message = prepare_minecraft(
+                task_data_path,
+                os.path.join(physio_data_path, physio_type),
+                experiment_info_path,
+                experiment,
+                physio_type,
+                synchronization_frequency,
+                os.path.join(output_dir, experiment, physio_type),
+                physio_type_info['interpolation_method']
+            )
+            string_stream.write(message)
+            if status:
+                experiments_tasks_data[physio_type][experiment]['minecraft'] = task_data
 
-            experiments_tasks_data[physio_type][experiment]['minecraft_training'] = {}
-
-            string_stream.write(f'[{experiment}] Preparing minecraft saturn A\n')
-
-            experiments_tasks_data[physio_type][experiment]['minecraft_saturn_a'] = {}
-
-            string_stream.write(f'[{experiment}] Preparing minecraft saturn B\n\n')
-
-            experiments_tasks_data[physio_type][experiment]['minecraft_saturn_b'] = {}
+            string_stream.write('\n')
 
     report_writer('preparing_preparation_report.txt', string_stream.getvalue(), to_terminal=verbose)
 
