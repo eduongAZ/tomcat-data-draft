71733f05663cfdd7aaec85fb8c9c8733fa251054
Eric Duong <eduong@arizona.edu>
2023-06-12 09:46:38 -0700

    process finger tapping

--- code/process/process_task_data.py
+++ code/process/process_task_data.py
@@ -3,18 +3,30 @@ import os
 from tqdm import tqdm
 
 from common import write_df
+from .process_finger_tapping import process_finger_tapping
 from .process_rest_state import process_rest_state
 
 
-def processing_rest_state(exp_data: dict[str, any]):
-    rest_state_df = process_rest_state(
-        exp_data['info'],
-        exp_data['task_csv_path'],
-        exp_data['physio'],
-        exp_data['frequency']
+def processing_rest_state(task_data: dict[str, any]):
+    task_df = process_rest_state(
+        task_data['info'],
+        task_data['task_csv_path'],
+        task_data['physio'],
+        task_data['frequency']
     )
-    os.makedirs(exp_data['output_dir'], exist_ok=True)
-    write_df(rest_state_df, os.path.join(exp_data['output_dir'], 'rest_state_physio_task.csv'))
+    os.makedirs(task_data['output_dir'], exist_ok=True)
+    write_df(task_df, os.path.join(task_data['output_dir'], f'rest_state_physio_task.csv'))
+
+
+def processing_finger_tapping(task_data: dict[str, any]):
+    task_df = process_finger_tapping(
+        task_data['info'],
+        task_data['task_csv_path'],
+        task_data['physio'],
+        task_data['frequency']
+    )
+    os.makedirs(task_data['output_dir'], exist_ok=True)
+    write_df(task_df, os.path.join(task_data['output_dir'], f'finger_tapping_physio_task.csv'))
 
 
 def process_task_data(task_data: dict[str, any]):
@@ -22,4 +34,5 @@ def process_task_data(task_data: dict[str, any]):
     for exp, exp_data in pbar:
         pbar.set_description(exp)
 
-        processing_rest_state(exp_data['rest_state'])
+        processing_rest_state(exp_data["rest_state"])
+        processing_finger_tapping(exp_data["finger_tapping"])
